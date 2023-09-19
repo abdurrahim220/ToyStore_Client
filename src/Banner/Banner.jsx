5731a5b9648c1db953a6628f5b3404d8047ac91f
abdurrahim220 <abdurrahim5@gmail.com>
2023-09-19 23:09:40 +0600

    banner problem fixed

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-
-
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 const Banner = () => {
-    return (
-        <div className="carousel w-full my-5">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img src={'https://i.ibb.co/ZLqrWJs/10.png'} className="w-full rounded-lg" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                </div>
+  const images = [
+    {
+      id: 10,
+      url: "https://i.ibb.co/ZLqrWJs/10.png",
+    },
+    {
+      id: 11,
+      url: "https://i.ibb.co/Q9XgCjZ/11.jpg",
+    },
+    {
+      id: 12,
+      url: "https://i.ibb.co/JrVYsxX/12.jpg",
+    },
+    {
+      id: 13,
+      url: "https://i.ibb.co/1rJzVWJ/13.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <Carousel className="">
+        {images.map((image, index) => {
+          return (
+            <div className="h-[650px]">
+              <img src={image.url} key={index} alt="" />
             </div>
-            <div id="slide2" className="carousel-item relative w-full">
-                <img src={'https://i.ibb.co/Q9XgCjZ/11.jpg'} className="w-full rounded-lg" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img src={'https://i.ibb.co/JrVYsxX/12.jpg'} className="w-full rounded-lg" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src={'https://i.ibb.co/1rJzVWJ/13.jpg'} className="w-full rounded-lg" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-        </div>
-    );
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
